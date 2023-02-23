25f54d9048dfa50d86f45795e8bc3da72cee2781
Kevin Salmeron <ksalmeron1993@icloud.com>
2023-02-23 14:34:34 -0800

    x

--- movies/main.py
+++ movies/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import os
+<<<<<<< Updated upstream
 from routers import movies, accounts
+=======
+from routers import movies, comments, bookmarks
+>>>>>>> Stashed changes
 
 app = FastAPI()
 
 app.include_router(movies.router)
+<<<<<<< Updated upstream
 app.include_router(accounts.router)
+=======
+app.include_router(comments.router)
+app.include_router(bookmarks.router)
+>>>>>>> Stashed changes
