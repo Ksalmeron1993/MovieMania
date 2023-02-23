b80e9257aa4ab963c1cfd8393e5041d8e5f45abd
Cierra Reese <creese737@gmail.com>
2023-02-23 16:40:24 -0700

    login form

--- ghi/src/pages/Login.js
+++ ghi/src/pages/Login.js
@@ -1,6 +1,33 @@
-//import
+//import React, { useState } from "react";
+//import { useNavigate } from "react-router-dom";
+// need to import a useToken from Authentication 
 
 function Login(){
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const login = useToken()[1];
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+            login(username, password);
+    }
+    function handleClick() {
+        navigate("/signup");
+    }
+
+
+    return (
+        <div className = "row">
+            <div className="offset-3 col-6">
+                <div className="shadow p-4 mt-4">
+                    <h1>Hello Movie Maniac Login!</h1>
+                    <form onSubmit={handleSubmit}>
+                        
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    );
 }
 export default Login
