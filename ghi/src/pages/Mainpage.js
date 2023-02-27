5de04c5049c614797cb956874446d292ab4a8c18
Cierra Reese <creese737@gmail.com>
2023-02-26 23:11:59 -0700

    update

--- ghi/src/pages/Mainpage.js
+++ ghi/src/pages/Mainpage.js
@@ -2,6 +2,11 @@
 // this is the page to the sign up + login forms
 
 function Mainpage(){
-    
+    return (
+        // image & sign up and login buttons
+        <div className="px-4 py-5 my-5 text-center">
+            <h1 className="display-5 fw-bold">Movie Mania</h1>
+        </div>
+    ) 
 }
 export default Mainpage
