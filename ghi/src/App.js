12b2df0b663b5e779e2ac38b7186da85b01ee47f
Uzair Suleman <uzairsuleman23@gmail.com>
2023-02-27 14:24:29 -0600

    new backend integration

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -84,72 +84,186 @@
 // export default App;
 
 
-import React, { useState } from "react";
-import { useEffect } from "react";
-import './styles/App.css';
-import SearchIcon from './search.svg';
-import MovieCard from "./MovieCard";
+// import React, { useState } from "react";
+// import { useEffect } from "react";
+// import './App.css';
+// import SearchIcon from './search.svg';
+// import MovieCard from "./MovieCard";
 
 
-//a5371ad6
-const API_URL = 'http://www.omdbapi.com?apikey=a5371ad6'
+// //a5371ad6
+// const API_URL = 'https://api.themoviedb.org/3/movie/{movie_name}?api_key=7d055fdafcdf398aab55d81760d1c151
+// '
 
-const App =() =>{
-    const [movies, setMovies] = useState();
-    const [searchTerm, setSearchTerm] = useState('');
-    const searchMovies = async(title) => {
-        const response = await fetch(`${API_URL}&s=${title}`)
-        const data = await response.json()
-        setMovies(data.Search)
+// const App =() =>{
+//     const [movies, setMovies] = useState();
+//     const [searchTerm, setSearchTerm] = useState('');
+//     const searchMovies = async(title) => {
+//         const response = await fetch(`${API_URL}&s=${title}`)
+//         const data = await response.json()
+//         setMovies(data.Search)
+//     }
+
+//     useEffect(() => {
+//         searchMovies('Marvel');
+
+
+//     },[]);
+
+
+//     return (
+//         <div className = 'app'>
+//             <h1>Movie Mania</h1>
+//             <div className ='search'>
+//                 <input
+//                     placeholder="Search for Movies"
+//                     value ={searchTerm}
+//                     onChange ={(e)=> setSearchTerm(e.target.value)}
+//                 />
+//                 <img
+//                     src= {SearchIcon}
+//                     alt = "search"
+//                     onClick ={()=> searchMovies(searchTerm)}
+
+//                 />
+
+//             </div>
+//             {
+//                 movies?.length > 0
+//                     ?(
+//                         <div className ='container'>
+//                             {movies.map((movie) =>(
+//                                 <MovieCard movie={movie} />
+//                             ))
+
+//                             }
+
+//                         </div>
+
+
+//                     ) : (
+//                         <div className ='empty'>
+//                             <h2>No Movies Found </h2>
+//                         </div>
+//                     )
+//             }
+
+//         </div>
+
+//     );
+// }
+
+// export default App
+
+// import React, { useState } from "react";
+// import { useEffect } from "react";
+// import './App.css';
+// import SearchIcon from './search.svg';
+// import MovieCard from "./MovieCard";
+
+
+
+// const API_URL = `https://api.themoviedb.org/3/search/movie?api_key=7d055fdafcdf398aab55d81760d1c151&query=`;
+
+// const App =() =>{
+//     const [movies, setMovies] = useState([]);
+//     const [searchTerm, setSearchTerm] = useState('');
+
+//     const searchMovies = async(title) => {
+//         const response = await fetch(`${API_URL}${title}`);
+//         const data = await response.json();
+//         setMovies(data.results);
+//     }
+
+//     useEffect(() => {
+//         searchMovies('marvel');
+//     },[]);
+
+//     return (
+//         <div className = 'app'>
+//             <h1>Movie Mania</h1>
+//             <div className ='search'>
+//                 <input
+//                     placeholder="Search for Movies"
+//                     value ={searchTerm}
+//                     onChange ={(e)=> setSearchTerm(e.target.value)}
+//                 />
+//                 <img
+//                     src= {SearchIcon}
+//                     alt = "search"
+//                     onClick ={()=> searchMovies(searchTerm),
+//                                 console.log(searchTerm)
+//                     }
+//                 />
+//             </div>
+//             {
+//                 movies?.length > 0
+//                     ?(
+//                         <div className ='container'>
+//                             {movies.map((movie) =>(
+//                                 <MovieCard key={movie.id} movie={movie} />
+//                             ))}
+//                         </div>
+//                     ) : (
+//                         <div className ='empty'>
+//                             <h2>No Movies Found </h2>
+//                         </div>
+//                     )
+//             }
+//         </div>
+//     );
+// }
+
+// export default App;
+
+import React, { useState } from 'react';
+import MovieCard from './MovieCard';
+import './App.css';
+
+function App() {
+  const [query, setQuery] = useState('');
+  const [movies, setMovies] = useState([]);
+
+  const searchMovies = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(
+        `http://localhost:8000/movies?query=${query}`
+      );
+      const data = await response.json();
+      setMovies(data.results);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    useEffect(() => {
-        searchMovies('Marvel');
-
-
-    },[]);
-
-
-    return (
-        <div className = 'app'>
-            <h1>Movie Mania</h1>
-            <div className ='search'>
-                <input
-                    placeholder="Search for Movies"
-                    value ={searchTerm}
-                    onChange ={(e)=> setSearchTerm(e.target.value)}
-                />
-                <img
-                    src= {SearchIcon}
-                    alt = "search"
-                    onClick ={()=> searchMovies(searchTerm)}
-
-                />
-
-            </div>
-            {
-                movies?.length > 0
-                    ?(
-                        <div className ='container'>
-                            {movies.map((movie) =>(
-                                <MovieCard movie={movie} />
-                            ))
-
-                            }
-
-                        </div>
-
-
-                    ) : (
-                        <div className ='empty'>
-                            <h2>No Movies Found </h2>
-                        </div>
-                    )
-            }
-
-        </div>
-
-    );
+  return (
+    <div className="container">
+      <h1 className="title">React Movie Search</h1>
+      <form className="form" onSubmit={searchMovies}>
+        <label className="label" htmlFor="query">
+          Movie Name
+        </label>
+        <input
+          className="input"
+          type="text"
+          name="query"
+          placeholder="i.e. Jurassic Park"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="button" type="submit">
+          Search
+        </button>
+      </form>
+      <div className="card-list">
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- movies/queries/movies.py
+++ movies/queries/movies.py
@@ -1,128 +1,143 @@
+# from pydantic import BaseModel
+# from typing import List, Optional, Union
+# from datetime import date
+# from queries.pool import pool
+
+
+# class Error(BaseModel):
+#     message: str
+
+
+# #what data do we need for submitting a movie
+# #data coming IN & and out of our endpoints in fastAPI - has nothing to do with our database
+# class MovieIn(BaseModel):
+#     title: str
+#     director: str
+#     release_date : date
+#     genre: str
+#     runtime: int
+#     plot_summary : str
+
+# class MovieOut(BaseModel):
+#     id: int
+#     title: str
+#     director: str
+#     release_date : date
+#     genre: str
+#     runtime: int
+#     plot_summary : str
+
+# class MovieRepository:
+#     def update_movie(self, movie_id: int, movie: MovieIn) -> Union[MovieOut, Error]:
+#         try:
+#             #Connect to the database
+#             with pool.connection() as conn:
+#                 # Get a cursor to run SQL with
+#                 with conn.cursor() as db:
+#                     db.execute(
+#                         """
+#                         UPDATE movies
+#                         SET title = %s
+#                         , director = %s
+#                         , release_date = %s
+#                         , genre = %s
+#                         , runtime = %s
+#                         , plot_summary = %s
+#                         WHERE id = %s
+#                         """,
+#                         [
+#                             movie.title,
+#                             movie.director,
+#                             movie.release_date,
+#                             movie.genre,
+#                             movie.runtime,
+#                             movie.plot_summary,
+#                             movie_id
+#                         ]
+#                     )
+#                     return self.movie_in_to_out(movie_id, movie)
+#         except Exception as e:
+#             print(e)
+#             return {"message": "Could not update that movie"}
+
+#     def get_all_movies(self) -> Union[Error, List[MovieOut]]:
+#         try:
+#             # Connect to the database
+#             with pool.connection() as conn:
+#                 # Get a cursor to run SQL with
+#                 with conn.cursor() as db:
+#                     # Execute the SELECT statement
+#                     db.execute(
+#                         """
+#                         SELECT id, title, director, release_date , genre, runtime, plot_summary
+#                         FROM movies
+#                         ORDER BY title;
+#                         """
+#                     )
+#                     return [
+#                         MovieOut(
+#                             id=record[0],
+#                             title=record[1],
+#                             director=record[2],
+#                             release_date=record[3],
+#                             genre=record[4],
+#                             runtime=record[5],
+#                             plot_summary=record[6]
+#                         )
+#                         for record in db
+#                     ]
+#         except Exception as e:
+#             print(e)
+#             return {"message": "Could not get all movies"}
+
+#     def create(self, movie: MovieIn) -> Union[MovieOut, Error]:
+#         try:
+#             # Connect to the database
+#             with pool.connection() as conn:
+#                 # Get a cursor to run SQL with
+#                 with conn.cursor() as db:
+#                     # Run our INSERT statement
+#                     result = db.execute(
+#                         """
+#                         INSERT INTO movies
+#                             (title, director, release_date, genre, runtime, plot_summary)
+#                         VALUES
+#                             (%s, %s, %s, %s, %s, %s)
+#                         RETURNING id;
+#                         """,
+#                         [
+#                             movie.title,
+#                             movie.director,
+#                             movie.release_date,
+#                             movie.genre,
+#                             movie.runtime,
+#                             movie.plot_summary
+#                         ]
+#                     )
+#                     id = result.fetchone()[0]
+#                     # Return new data
+#                     return self.movie_in_to_out(id, movie)
+#         except Exception:
+#             return {"message": "Create did not work properly"}
+
+#     def movie_in_to_out(self, id: int, movie: MovieIn) -> MovieOut:
+#         old_data = movie.dict()
+#         return MovieOut(id=id, **old_data)
+
+from typing import List, Optional
 from pydantic import BaseModel
-from typing import List, Optional, Union
-from datetime import date 
-from queries.pool import pool
 
-
-class Error(BaseModel):
-    message: str
-
-
-#what data do we need for submitting a movie 
-#data coming IN & and out of our endpoints in fastAPI - has nothing to do with our database
-class MovieIn(BaseModel):
-    title: str
-    director: str
-    release_date : date 
-    genre: str 
-    runtime: int
-    plot_summary : str
-
-class MovieOut(BaseModel):
+class Movie(BaseModel):
     id: int
     title: str
-    director: str
-    release_date : date 
-    genre: str 
-    runtime: int
-    plot_summary : str
+    overview: str
+    poster_path: Optional[str]
+    release_date: Optional[str]
+    vote_average: Optional[float]
 
-class MovieRepository: 
-    def update_movie(self, movie_id: int, movie: MovieIn) -> Union[MovieOut, Error]:
-        try:
-            #Connect to the database 
-            with pool.connection() as conn:
-                # Get a cursor to run SQL with
-                with conn.cursor() as db: 
-                    db.execute(
-                        """
-                        UPDATE movies
-                        SET title = %s
-                        , director = %s
-                        , release_date = %s
-                        , genre = %s
-                        , runtime = %s 
-                        , plot_summary = %s
-                        WHERE id = %s
-                        """,
-                        [
-                            movie.title,
-                            movie.director,
-                            movie.release_date,
-                            movie.genre,
-                            movie.runtime, 
-                            movie.plot_summary,
-                            movie_id
-                        ]
-                    )
-                    return self.movie_in_to_out(movie_id, movie)
-        except Exception as e:
-            print(e)
-            return {"message": "Could not update that movie"}
-
-    def get_all_movies(self) -> Union[Error, List[MovieOut]]:
-        try:
-            # Connect to the database 
-            with pool.connection() as conn:
-                # Get a cursor to run SQL with
-                with conn.cursor() as db: 
-                    # Execute the SELECT statement 
-                    db.execute(
-                        """
-                        SELECT id, title, director, release_date , genre, runtime, plot_summary
-                        FROM movies 
-                        ORDER BY title;
-                        """
-                    )
-                    return [
-                        MovieOut(
-                            id=record[0],
-                            title=record[1],
-                            director=record[2],
-                            release_date=record[3],
-                            genre=record[4],
-                            runtime=record[5],
-                            plot_summary=record[6]
-                        )
-                        for record in db 
-                    ]
-        except Exception as e:
-            print(e)
-            return {"message": "Could not get all movies"}      
-    
-    def create(self, movie: MovieIn) -> Union[MovieOut, Error]:
-        try:
-            # Connect to the database 
-            with pool.connection() as conn:
-                # Get a cursor to run SQL with
-                with conn.cursor() as db:
-                    # Run our INSERT statement
-                    result = db.execute(
-                        """
-                        INSERT INTO movies
-                            (title, director, release_date, genre, runtime, plot_summary)
-                        VALUES
-                            (%s, %s, %s, %s, %s, %s)
-                        RETURNING id;
-                        """,
-                        [
-                            movie.title, 
-                            movie.director, 
-                            movie.release_date, 
-                            movie.genre, 
-                            movie.runtime, 
-                            movie.plot_summary
-                        ]
-                    )
-                    id = result.fetchone()[0]
-                    # Return new data
-                    return self.movie_in_to_out(id, movie)
-        except Exception:
-            return {"message": "Create did not work properly"}
-
-    def movie_in_to_out(self, id: int, movie: MovieIn) -> MovieOut:
-        old_data = movie.dict()
-        return MovieOut(id=id, **old_data)
-
-
+class MovieSearchResult(BaseModel):
+    page: int
+    total_results: int
+    total_pages: int
+    results: List[Movie]
 
