734b4ceac2a54f73c14c3835730d16d5fda0f6d1
Maloneym16 <105671828+Maloneym16@users.noreply.github.com>
2023-03-01 20:12:18 -0500

    Everything but post

--- movies/tests/test_movies.py
+++ movies/tests/test_movies.py
@@ -1,45 +1,45 @@
-from fastapi.testclient import TestClient # check fast api documentation for overrides
-from main import app
-from queries.users import UsersRepo
-# from queries.bookmarks import BookmarkRepository
+# from fastapi.testclient import TestClient # check fast api documentation for overrides
+# from main import app
+# from queries.users import UsersRepo
+# # from queries.bookmarks import BookmarkRepository
 
-client = TestClient(app)
+# client = TestClient(app)
 
-class EmptyUserQueries:
-    def get_all(self):
-        return []
-    
-# class EmptyBookmarkQueries:
+# class EmptyUserQueries:
 #     def get_all(self):
 #         return []
-    
-# class CreateBookmark:
-#     def create(self, bookmark):
-#         result = {
-#             "id":
-#             ""
-#         }
-#         return result
+
+# # class EmptyBookmarkQueries:
+# #     def get_all(self):
+# #         return []
+
+# # class CreateBookmark:
+# #     def create(self, bookmark):
+# #         result = {
+# #             "id":
+# #             ""
+# #         }
+# #         return result
 
 
 
-def test_get_all_users():
-    # Arrange
-    app.dependency_overrides[UsersRepo] = EmptyUserQueries
-    response = client.get("/get/all")
-    # Act 
-    app.dependency_overrides = {}
-    # Assert
-    assert response.status_code == 200
-    assert response.json() == {"users": []}
-
-
-# def test_get_all_bookmarks():
+# def test_get_all_users():
 #     # Arrange
-#     app.dependency_overrides[BookmarkRepository] = EmptyBookmarkQueries
-#     response = client.get("/movies/bookmarks/")
-#     # Act 
+#     app.dependency_overrides[UsersRepo] = EmptyUserQueries
+#     response = client.get("/get/all")
+#     # Act
 #     app.dependency_overrides = {}
 #     # Assert
 #     assert response.status_code == 200
-#     assert response.json() == {"bookmarks": []}
+#     assert response.json() == {"users": []}
+
+
+# # def test_get_all_bookmarks():
+# #     # Arrange
+# #     app.dependency_overrides[BookmarkRepository] = EmptyBookmarkQueries
+# #     response = client.get("/movies/bookmarks/")
+# #     # Act
+# #     app.dependency_overrides = {}
+# #     # Assert
+# #     assert response.status_code == 200
+# #     assert response.json() == {"bookmarks": []}
--- movies/tests/test_users.py
+++ movies/tests/test_users.py
@@ -1,6 +1,6 @@
 from fastapi.testclient import TestClient
 from main import app
-from queries.users import UsersRepo
+from queries.users import UsersRepo, UsersIn
 
 client = TestClient(app)
 
@@ -15,7 +15,8 @@ class FakeUsersRepo:
         }
 
 
-def test_get_all():
+
+def test_get_user_by_id():
     app.dependency_overrides[UsersRepo] = FakeUsersRepo
     response = client.get(
         "/users/get/1",
@@ -28,3 +29,41 @@ def test_get_all():
             "email": "apple@sauce.com",
             "username": "apple",
         }
+
+
+
+class CreateUserQueries:
+    def create(self, user, hashed_password):
+        result = {
+            "first_name": "Apple",
+            "last_name": "Sauce",
+            "email": "apple@sauce.com",
+            "username": "apple",
+        }
+        result.update(user)
+        return result
+
+def test_create_user():
+    app.dependency_overrides[UsersRepo] = CreateUserQueries
+
+    json = {
+        "first_name": "Apple",
+        "last_name": "Sauce",
+        "email": "apple@sauce.com",
+        "username": "apple",
+        "password": "password",
+    }
+
+    expected = {
+        "id": 1,
+        "first_name": "Apple",
+        "last_name": "Sauce",
+        "email": "apple@sauce.com",
+        "username": "apple",
+        "hashed_password": "$2b$12$UTzkAvfrMs0.EEicbdn4BuOnZBWJybTSpcgU9OkkJ.ScTon/bUijq",
+    }
+
+    response = client.post("/signup", json=json)
+    app.dependency_overrides = {}
+    assert response.status_code == 200
+    assert response.json() == expected
