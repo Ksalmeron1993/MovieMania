98840adb99d3a473fa4969e5476f5cbcb5a78136
mark <yrt3ry@gmail.com>
2023-03-02 11:45:03 -0800

    Revert "create an endpoint for popular movies, and made it so that the default movies..."
    This reverts commit b8cdd2281a89ce35b48e8d928e5f3d09926ba47e.

--- ghi/src/pages/Accountpage.js
+++ ghi/src/pages/Accountpage.js
@@ -1,24 +1,7 @@
-// //import
-// // import React, { useState, useEffect } from "react";
-// // import { useNavigate } from "react-router-dom";
-// // import Nav
-// // import a token from Authentication
+//import
 
+function Accountpage (){
 
-// // function Accountpage (){
-// //     const [accountDetails, setAccountDetails] = useState("");
-// //     const [username, setUsername] = useState("");
-// //     const [updateUsername, setUpdateUsername] = useState();
-// //     const [, token] = useToken();
+}
 
-// //     useEffect(() => {
-// //         async function fetchAccountDetails() {}
-// //     });
-
-// //     const handleUsernameUpdate = async (e) => {
-// //         e.preventDefault();
-
-// //     }
-
-// // }
 export default Accountpage
--- movies/authenticator.py
+++ movies/authenticator.py
@@ -1,28 +1,2 @@
-# authenticator.py
 import os
 from fastapi import Depends
-from jwtdown_fastapi.authentication import Authenticator
-from queries.users import UsersRepo, UsersOut, UsersOutWithPassword
-
-class MyAuthenticator(Authenticator):
-    async def get_account_data(
-            self,
-            username: str,
-            users: UsersRepo,
-    ):
-         return users.get_user(username)
-
-    def get_account_getter(
-        self,
-        users: UsersRepo = Depends(),
-    ):
-        return users
-
-    def get_hashed_password(self, user: UsersOutWithPassword):
-        return user.hashed_password
-
-    def get_users_data_for_cookie(self, users: UsersOut):
-        return users.username, UsersOut(**users.dict())
-
-
-authenticator = MyAuthenticator(os.environ["SIGNING_KEY"])
