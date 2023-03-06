45efa65fc6f2ee56c15a800ffb1a9ebf9fe9c6d0
Cierra Reese <creese737@gmail.com>
2023-03-06 16:58:04 -0700

    created test for get all users, it passed

--- movies/tests/test_users.py
+++ movies/tests/test_users.py
@@ -32,38 +32,80 @@ def test_get_user_by_id():
 
 
 
-class CreateUserQueries:
-    def create(self, user, hashed_password):
-        result = {
-            "first_name": "Apple",
-            "last_name": "Sauce",
-            "email": "apple@sauce.com",
-            "username": "apple",
-        }
-        result.update(user)
-        return result
+# class CreateUserQueries:
+#     def create(self, user, hashed_password):
+#         result = {
+#             "first_name": "Apple",
+#             "last_name": "Sauce",
+#             "email": "apple@sauce.com",
+#             "username": "apple",
+#         }
+#         result.update(user)
+#         return result
 
-def test_create_user():
-    app.dependency_overrides[UsersRepo] = CreateUserQueries
+# def test_create_user():
+#     app.dependency_overrides[UsersRepo] = CreateUserQueries
 
-    json = {
-        "first_name": "Apple",
-        "last_name": "Sauce",
-        "email": "apple@sauce.com",
-        "username": "apple",
-        "password": "password",
-    }
+#     json = {
+#         "first_name": "Apple",
+#         "last_name": "Sauce",
+#         "email": "apple@sauce.com",
+#         "username": "apple",
+#         "password": "password",
+#     }
 
-    expected = {
-        "id": 1,
-        "first_name": "Apple",
-        "last_name": "Sauce",
-        "email": "apple@sauce.com",
-        "username": "apple",
-        "hashed_password": "$2b$12$UTzkAvfrMs0.EEicbdn4BuOnZBWJybTSpcgU9OkkJ.ScTon/bUijq",
-    }
+#     expected = {
+#         "id": 1,
+#         "first_name": "Apple",
+#         "last_name": "Sauce",
+#         "email": "apple@sauce.com",
+#         "username": "apple",
+#         "hashed_password": "$2b$12$UTzkAvfrMs0.EEicbdn4BuOnZBWJybTSpcgU9OkkJ.ScTon/bUijq",
+#     }
 
-    response = client.post("/signup", json=json)
-    app.dependency_overrides = {}
+#     response = client.post("/signup", json=json)
+#     app.dependency_overrides = {}
+#     assert response.status_code == 200
+#     assert response.json() == expected
+
+
+class FakeAllUserRepo:
+    def get_all_users(self):
+        return [
+            {
+                "id": 1,
+                "first_name": "po",
+                "last_name": "po",
+                "email": "po@pandas.com",
+                "username": "panda",
+            },
+            {
+                "id": 2,
+                "first_name": "tigress",
+                "last_name": "tigress",
+                "email": "tigress@tigers.com",
+                "username": "tiger",
+            }
+        ]
+def test_all_users():
+    app.dependency_overrides[UsersRepo] = FakeAllUserRepo
+    response = client.get(
+        "/get/all",
+    )
     assert response.status_code == 200
-    assert response.json() == expected
+    assert response.json() == [
+        {
+            "id": 1,
+            "first_name": "po",
+            "last_name": "po",
+            "email": "po@pandas.com",
+            "username": "panda",
+        },
+        {
+            "id": 2,
+            "first_name": "tigress",
+            "last_name": "tigress",
+            "email": "tigress@tigers.com",
+            "username": "tiger",
+        }
+    ]
