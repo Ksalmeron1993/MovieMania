f2d3bd372635821618839c4dd4a2f30a0031381b
Maloneym16 <105671828+Maloneym16@users.noreply.github.com>
2023-03-10 11:20:59 -0500

    miketest

--- movies/tests/test_bookmarks.py
+++ movies/tests/test_bookmarks.py
@@ -1,42 +1,24 @@
-# from fastapi.testclient import TestClient # check fast api documentation for overrides
-# from main import app
-# from queries.users import UsersRepo
-# # from queries.bookmarks import BookmarkRepository
+from fastapi.testclient import TestClient # check fast api documentation for overrides
+from main import app
+from queries.bookmarks import BookmarkRepository
+from authenticator import authenticator
 
-# client = TestClient(app)
+client = TestClient(app)
 
-# class EmptyUserQueries:
-#     def get_all(self):
-#         return []
 
-# # class EmptyBookmarkQueries:
-# #     def get_all(self):
-# #         return []
-
-# # class CreateBookmark:
-# #     def create(self, bookmark):
-# #         result = {
-# #             "id":
-# #             ""
-# #         }
-# #         return result
-
-# def test_get_all_users():
-#     # Arrange
-#     app.dependency_overrides[UsersRepo] = EmptyUserQueries
-#     response = client.get("/get/all")
-#     # Act
-#     app.dependency_overrides = {}
-#     # Assert
-#     assert response.status_code == 200
-#     assert response.json() == {"users": []}
-
-# # def test_get_all_bookmarks():
-# #     # Arrange
-# #     app.dependency_overrides[BookmarkRepository] = EmptyBookmarkQueries
-# #     response = client.get("/movies/bookmarks/")
-# #     # Act
-# #     app.dependency_overrides = {}
-# #     # Assert
-# #     assert response.status_code == 200
-# #     assert response.json() == {"bookmarks": []}
+class FakeBookmarkIDRepo:
+    def get_bookmark_by_id(self, id):
+        return {
+        "user_id": 1,
+        "movie_id": 1234,
+        }
+def test_get_a_bookmark():
+    app.dependency_overrides[BookmarkRepository] = FakeBookmarkIDRepo
+    response = client.get(
+        "/bookmarks/get/1",
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "user_id": 1,
+        "movie_id": 1234,
+}
---
+++ movies/tests/test_mike.py
@@ -0,0 +1,32 @@
+from fastapi.testclient import TestClient
+from main import app
+from queries.users import UsersRepo, UsersIn
+from authenticator import authenticator
+
+client = TestClient(app)
+
+class FakeUsersRepo:
+    def get_user_by_id(self, id):
+        return {
+            "id": 1,
+            "first_name": "Apple",
+            "last_name": "Sauce",
+            "email": "apple@sauce.com",
+            "username": "apple",
+        }
+
+
+
+def test_get_user_by_id():
+    app.dependency_overrides[UsersRepo] = FakeUsersRepo
+    response = client.get(
+        "/users/get/1",
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+            "id": 1,
+            "first_name": "Apple",
+            "last_name": "Sauce",
+            "email": "apple@sauce.com",
+            "username": "apple",
+        }
